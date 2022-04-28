c9b8afb174201cbc38d343e51c168cdfc6cdf80a
Nairukan <Draniki_das1>
2022-04-28 23:37:07 +0300

    Upgrade File Structure of Project
    Signed-off-by: Nairukan <Draniki_das1>

--- MermoryGame/Tools/IObserver.cs
+++ MermoryGame/Tools/IObserver.cs
@@ -2,6 +2,7 @@
 {
     public interface IObserver //Интерфейс наблюдателя
     {
+        string ObsName { get; set; }
         void Update(object[] ob); //Фунция принятия обновления наблюдаемого объекта с какими угодно параметрами
     }
 }
