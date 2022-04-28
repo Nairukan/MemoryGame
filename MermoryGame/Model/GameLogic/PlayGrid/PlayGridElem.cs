64767641ba69e7e028d88bc6fc1a688086ef6fbc
Nairukan <Draniki_das1>
2022-04-28 23:48:08 +0300

    The module cell of PlayGrid
    Signed-off-by: Nairukan <Draniki_das1>

---
+++ MermoryGame/Model/GameLogic/PlayGrid/PlayGridElem.cs
@@ -0,0 +1,27 @@
+﻿namespace MermoryGame.Control
+{
+    public class PlayGridElem
+    {
+        public enum Movement
+        {
+            Left,
+            Up,
+            Right,
+            Down
+        }
+
+        public enum Сonditions
+        {
+            Free,
+            Boarder,
+            Busy,
+            ForMove
+        }
+
+        public Movement MovingDirection;
+
+        public Сonditions PlateStatus;
+        public uint PrevValue;
+        public PlayCard WhoIsBusy;
+    }
+}
